6221b475332a985f04c79337380e29d95805b302
Jared Amen <jared.amen@kantata.com>
2022-06-08 16:25:49 -0600

    gilded rose week 2

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,10 +1,18 @@
 def update_quality(items)
   items.each do |item|
     if item.name == 'Conjured Mana Cake'
-      c = ConjuredItem.new(item)
-
-      c.update_quality
+      ConjuredItem.new(item).update_item
+      next
     end
+    if item.name == 'NORMAL ITEM'
+      NormalItem.new(item).update_item
+      next
+    end
+    if item.name == 'Aged Brie'
+      AgedBrieItem.new(item).update_item
+      next
+    end
+
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         if item.name != 'Sulfuras, Hand of Ragnaros'
@@ -51,16 +59,54 @@ def update_quality(items)
   end
 end
 
-class ConjuredItem
+class ItemUpdater
   attr_reader :item
 
   def initialize(item)
     @item = item
   end
 
+  def update_item
+    update_sell_in
+    update_quality
+  end
+end
+
+class AgedBrieItem < ItemUpdater
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
+  def update_quality
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.sell_in < 0 && item.quality < 50
+  end
+end
+
+class NormalItem < ItemUpdater
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
   def update_quality
     item.quality -= 1 if item.quality > 0
     item.quality -= 1 if item.quality > 0 && item.sell_in <= 0
+  end
+end
+
+class ConjuredItem < ItemUpdater
+  def update_item
+    update_sell_in
+    update_quality
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
+  def update_quality
+    item.quality -= 2 if item.quality > 0
+    item.quality -= 2 if item.quality > 0 && item.sell_in <= 0
   end
 end
 
