dbe9755cad5880660fe0fc7527609cd242d02b9a
Jared Nielson <jnielson@mavenlink.com>
2022-06-08 15:42:58 -0600

    Week 1 mob

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,10 @@
 def update_quality(items)
   items.each do |item|
+    if item.name == 'Conjured Mana Cake'
+      c = ConjuredItem.new(item)
+
+      c.update_quality
+    end
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         if item.name != 'Sulfuras, Hand of Ragnaros'
@@ -43,6 +48,19 @@ def update_quality(items)
         end
       end
     end
+  end
+end
+
+class ConjuredItem
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update_quality
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 1 if item.quality > 0 && item.sell_in <= 0
   end
 end
 
