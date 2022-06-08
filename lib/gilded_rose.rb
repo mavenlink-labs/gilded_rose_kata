d6ddd3c5c57cb48c3e3f9074fe8adab9840080c0
ashersorensen <asorensen@mavenlink.com>
2022-06-08 16:24:53 -0600

    Squashed commit of the following:
    commit f304c2253c1b273130e685a982ee6e490938050a
    Author: ashersorensen <asorensen@mavenlink.com>
    Date:   Wed Jun 8 16:24:42 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit bd3894f60f8dcc83e1bbe72aa1cd2d6dd4b41b5a
    Author: ashersorensen <asorensen@mavenlink.com>
    Date:   Wed Jun 8 16:11:40 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 086829501a35bd37b2488e3de9e4a20db2cb6255
    Author: George Wang <gwang@mavenlink.com>
    Date:   Wed Jun 8 15:04:50 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:spec/gilded_rose_spec.rb
    
    commit 550b2ea41e1df1e64ed08bfe61659aa6118b8a8f
    Author: Dario Mercado <dmercado@mavenlink.com>
    Date:   Wed Jun 8 14:58:58 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 71e5e4189e70afc0b04fa5cfd328f924a5148992
    Author: dezkowalski <dezkowalski@gmail.com>
    Date:   Wed Jun 8 15:52:27 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 43426cd9f886ede9d909f9ee51a3ec0d2853cdac
    Author: ashersorensen <asorensen@mavenlink.com>
    Date:   Wed Jun 8 15:45:56 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit f358104c54ba674bc0c2c8dd39433abca9ffa2db
    Author: George Wang <gwang@mavenlink.com>
    Date:   Wed May 25 15:10:39 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:lib/gilded_rose.rb
    
    commit f1504a4f95e03142384aa45aa01a7f0cc4bd2ba8
    Author: Dario Mercado <dmercado@mavenlink.com>
    Date:   Wed May 25 15:04:00 2022 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 419f3925f81d3c4379c33bcf28fe93a4e4ee583f
    Author: ashersorensen <asorensen@mavenlink.com>
    Date:   Wed May 25 15:46:11 2022 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: George Wang <gwang@mavenlink.com>
    Co-authored-by: dezkowalski <dezkowalski@gmail.com>
    Co-authored-by: Dario Mercado <dmercado@mavenlink.com>

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,48 +1,108 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
+class NormalItem
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def quality
+    if item.quality > 0
+      item.quality -= 1
+    end
+  end
+
+  def sell_in
+    item.sell_in -= 1
+    if item.sell_in < 0 && item.quality > 0
+      item.quality -= 1
+    end
+  end
+
+  def update_quality
+    quality
+    sell_in
+  end
+end
+
+class AgedBrie < NormalItem
+  def quality
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
+
+  def sell_in
+    item.sell_in -= 1
+    if item.sell_in < 0 && item.quality < 50
+      item.quality += 1
+    end
+  end
+end
+
+class BackstagePasses < NormalItem
+  def quality
+    if item.quality < 50
+      item.quality += 1
+      if item.sell_in < 11 && item.quality < 50
         item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
+      end
+      if item.sell_in < 6 && item.quality < 50
+        item.quality += 1
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
+  end
+
+  def sell_in
+    item.sell_in -= 1
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      item.quality = item.quality - item.quality
+    end
+  end
+end
+
+class Sulfuras < NormalItem
+  def quality
+    item.quality
+  end
+
+  def sell_in
+    item.sell_in
+  end
+end
+
+class ConjuredItem < NormalItem
+  def quality
+    if item.quality > 0
+      item.quality -= 2
+    end
+
+    if item.quality < 0
+      item.quality = 0
+    end
+  end
+
+  def sell_in
+    item.sell_in -= 1
+    if item.sell_in < 0 && item.quality > 0
+      item.quality -= 2
+
+      if item.quality < 0
+        item.quality = 0
       end
     end
+  end
+end
+
+def update_quality(items)
+  itemLookup = {
+    'Aged Brie' => AgedBrie,
+    'Backstage passes to a TAFKAL80ETC concert' => BackstagePasses,
+    'Sulfuras, Hand of Ragnaros' => Sulfuras,
+    'Conjured Mana Cake' => ConjuredItem
+  }
+
+  items.each do |item|
+    itemLookup.fetch(item.name, NormalItem).new(item).update_quality
   end
 end
 
