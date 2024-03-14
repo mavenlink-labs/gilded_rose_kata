f17d65389d854f1e83852af9a65461115098ea9d
Jared Nielson <jnielson@mavenlink.com>
2024-03-14 11:44:54 -0600

    ref: extract method for update_item_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -24,31 +24,35 @@ def decrement_sell_in(item)
   end
 end
 
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
+def update_item_quality(item)
+  if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if item.quality > 0
+      if item.name != 'Sulfuras, Hand of Ragnaros'
+        item.quality -= 1
       end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
+    end
+  else
+    if item.quality < 50
+      item.quality += 1
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality += 1
           end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality += 1
           end
         end
       end
     end
+  end
+end
+
+def update_quality(items)
+  items.each do |item|
+    update_item_quality(item)
     decrement_sell_in(item)
     update_expired_item(item)
   end
