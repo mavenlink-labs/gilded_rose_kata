855b405f8e52d4333d5d0abba479cd5b5a6571c0
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:02:48 -0600

    ref: consolidate conditional

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,10 +5,8 @@ def update_quality(items)
     end
     if !(item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') && item.quality < 50
       item.quality += 1
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 11
-        if item.quality < 50
-          item.quality += 1
-        end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 11 && item.quality < 50
+        item.quality += 1
       end
       if item.name == 'Backstage passes to a TAFKAL80ETC concert'
         if item.sell_in < 6
