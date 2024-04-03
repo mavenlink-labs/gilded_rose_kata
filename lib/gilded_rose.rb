ea7ed911e81db546c3b585c107440803df26dd25
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:52:51 -0600

    equivalent syntax

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,19 +5,11 @@ def update_quality(items)
       item.sell_in -= 1
       item.quality += 1 if item.sell_in < 0 && item.quality < 50
     elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality < 50
-        item.quality += 1
-      end
-      if item.quality < 50 && item.sell_in < 11
-        item.quality += 1
-      end
-      if item.quality < 50 && item.sell_in < 6
-        item.quality += 1
-      end
+      item.quality += 1 if item.quality < 50
+      item.quality += 1 if item.quality < 50 && item.sell_in < 11
+      item.quality += 1 if item.quality < 50 && item.sell_in < 6
       item.sell_in -= 1
-      if item.sell_in < 0
-        item.quality = 0
-      end
+      item.quality = 0 if item.sell_in < 0
     elsif item.name != 'Sulfuras, Hand of Ragnaros'
       if item.quality > 0
         item.quality -= 1
