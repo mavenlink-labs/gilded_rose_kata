493798669691eb0830b130c5cdeb0fddefb674bf
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:23:37 -0600

    rearrange conditional

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,6 +1,6 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros'
+    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros' && item.quality > 0
       item.quality -= 1
     end
     if item.name == 'Aged Brie' && item.quality < 50
