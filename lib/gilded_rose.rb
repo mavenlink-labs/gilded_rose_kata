8cb0eed79e8aa30726c3d0e2465d9ccc1c091eb2
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:43:59 -0600

    introduce duplication

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,20 +11,22 @@ def update_quality(items)
         item.quality += 1
       end
     end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
-      item.quality += 1
-    end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50 && item.sell_in < 11 && item.quality < 50
-      item.quality += 1
-    end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50 && item.sell_in < 6 && item.quality < 50
-      item.quality += 1
-    end
     if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-      item.sell_in -= 1
-    end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 0
-      item.quality = 0
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
+        item.quality += 1
+      end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50 && item.sell_in < 11 && item.quality < 50
+        item.quality += 1
+      end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50 && item.sell_in < 6 && item.quality < 50
+        item.quality += 1
+      end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        item.sell_in -= 1
+      end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 0
+        item.quality = 0
+      end
     end
     if !(item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros') && item.quality > 0
       item.quality -= 1
