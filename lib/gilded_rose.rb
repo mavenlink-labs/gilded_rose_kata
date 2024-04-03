e5a7d9b592264c02f7dc62de92f26657b33e7639
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:25:28 -0600

    rearrange

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,7 +18,7 @@ def update_quality(items)
     if item.name != 'Sulfuras, Hand of Ragnaros'
       item.sell_in -= 1
     end
-    if item.sell_in < 0 && item.name != "Aged Brie" && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros'
+    if item.sell_in < 0 && item.name != "Aged Brie" && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros' && item.quality > 0
       item.quality -= 1
     end
     if item.sell_in < 0 && item.name == 'Backstage passes to a TAFKAL80ETC concert'
