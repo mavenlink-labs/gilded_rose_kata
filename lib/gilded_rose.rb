a4184c1b1f8ab9568ae01805e1dd62756a98c962
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:22:22 -0600

    split conditional

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,7 +3,10 @@ def update_quality(items)
     if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros'
       item.quality -= 1
     end
-    if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') && item.quality < 50
+    if item.name == 'Aged Brie' && item.quality < 50
+      item.quality += 1
+    end
+    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
       item.quality += 1
     end
     if item.quality < 50 && item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 11 && item.quality < 50
