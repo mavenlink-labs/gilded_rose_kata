b8edefed7a2534f491d59851ee5d6adb79ecd92f
Andrew Aposhian <aaposhian@mavenlink.com>
2024-04-03 17:43:05 -0600

    slide statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,9 +9,6 @@ def update_quality(items)
     if item.name == 'Aged Brie' && item.sell_in < 0 && item.quality < 50
       item.quality += 1
     end
-    if !(item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros') && item.quality > 0
-      item.quality -= 1
-    end
     if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
       item.quality += 1
     end
@@ -21,17 +18,20 @@ def update_quality(items)
     if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50 && item.sell_in < 6 && item.quality < 50
       item.quality += 1
     end
-    if !(item.name == "Aged Brie" || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros')
+    if item.name == 'Backstage passes to a TAFKAL80ETC concert'
       item.sell_in -= 1
     end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 0
+      item.quality = 0
+    end
+    if !(item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros') && item.quality > 0
+      item.quality -= 1
+    end
+    if !(item.name == "Aged Brie" || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros')
       item.sell_in -= 1
     end
     if !(item.name == "Aged Brie" || item.name == 'Backstage passes to a TAFKAL80ETC concert' || item.name == 'Sulfuras, Hand of Ragnaros') && item.quality > 0 && item.sell_in < 0 &&
       item.quality -= 1
-    end
-    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sell_in < 0
-      item.quality = 0
     end
   end
 end
